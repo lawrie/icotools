f193fa0dc0480c79a023fbbb6a03f7afc25b92c3
lawrie <lawrie.griffiths@ntlworld.com>
2018-06-07 14:12:18 +0100

    Improved mod_vga_text and example

--- icosoc/mod_vga_text/mod_vga_text.py
+++ icosoc/mod_vga_text/mod_vga_text.py
@@ -4,7 +4,12 @@ def generate_c_code(icosoc_h, icosoc_c, mod):
 static inline void icosoc_@name@_setchar(uint8_t x, uint8_t y, uint8_t c)
 {
     *(volatile uint32_t*)(0x20000000 + @addr@ * 0x10000) = 
-		(((y << 6) + x) << 8) + c;
+		(((y * 80) + x) << 8) + c;
+}
+
+static inline void icosoc_@name@_setcolor(uint32_t color)
+{
+    *(volatile uint32_t*)(0x20000004 + @addr@ * 0x10000) = color;
 }
 """
     code = code.replace("@name@", mod["name"])
--- icosoc/mod_vga_text/mod_vga_text.v
+++ icosoc/mod_vga_text/mod_vga_text.v
@@ -17,6 +17,7 @@ module icosoc_mod_vga_text (
 
 	reg [15:0] vga;
 	wire clk_100;
+	reg [11:0] color = 12'hf00;
 
 	SB_IO #(
 		.PIN_TYPE(6'b 0110_01),
@@ -26,15 +27,19 @@ module icosoc_mod_vga_text (
 		.D_OUT_0(vga)
 	);
 
-	reg[7:0] x, y, char;
 	wire we;
-	wire [7:0] data_in, data_out;
+	wire [7:0] data_in; 
+        reg [7:0] data_out, data_out1;
 	reg [15:0] r_addr, w_addr;
-	reg [7:0] ascii;
+	wire [7:0] ascii;
 
 	always @(posedge clk100) begin
-		r_addr <= ((yb >> 4) << 6) + (xb >> 3);
-		if (data_out != 0) ascii <= data_out;
+		// Multiply y by 80 and add x
+		r_addr <= ((yb >> 4) << 6) + ((yb >> 4) << 4)+ (xb >> 3);
+		data_out1 <= data_out;
+		ascii <= data_out1;
+		pixel2 <= pixel1;
+		pixel <= pixel2;
 	end
 
 	always @(posedge clk) begin
@@ -49,7 +54,7 @@ module icosoc_mod_vga_text (
 					w_addr <= ctrl_wdat[23:8];
 					data_in <= ctrl_wdat[7:0];
 					we <= 1;
-				end
+				end else if (ctrl_addr == 4) color <= ctrl_wdat;
 			end
 
 		end
@@ -57,21 +62,22 @@ module icosoc_mod_vga_text (
 
         wire [9:0] xb, yb;
 	wire pixel;
-	wire show_text = data_out > 0 && pixel;
+	reg pixel1, pixel2;
+	wire blank;
 
-        assign vga[3:0] = show_text?15:0; // red
-	assign vga[7:4] = show_text?15:0; // blue
-	assign vga[11:8] = show_text?15:0; // green
+        assign vga[3:0] = pixel & !blank?color[11:8]:0; // red
+	assign vga[7:4] = pixel & !blank?color[7:4]:0; // blue
+	assign vga[11:8] = pixel & !blank?color[3:0]:0; // green
 	assign vga[14] = 0;
 	assign vga[15] = 0;
 
-	vga vga0 (.CLK(clk100), .HS(vga[12]), .VS(vga[13]), .x(xb), .y(yb));
+	vga vga0 (.CLK(clk100), .HS(vga[12]), .VS(vga[13]), .x(xb), .y(yb), .blank(blank));
 
 	vga_buff vb (.clk(clk100), .we(we), .r_addr(r_addr), .w_addr(w_addr),
 		     .data_in(data_in), .data_out(data_out));
 
 	font8x16 f (.ascii_code(ascii), .row(yb[3:0]), .col(xb[2:0]), 
-                       .pixel(pixel));
+                       .pixel(pixel1));
 endmodule
 
 module vga(
@@ -90,7 +96,7 @@ reg [15:0] prescaler = 0;
 assign blank = ((xc < 192) | (xc > 832) | (y > 479));
 assign HS = ~ ((xc > 23) & (xc < 65));
 assign VS = ~ ((y > 489) & (y < 493));
-assign x = ((xc < 192)?0:(xc - 192));
+assign x = ((xc < 192)?0:(xc-192));
 
 always @(posedge CLK)
 begin
@@ -123,10 +129,10 @@ module vga_buff(
   input [15:0] r_addr,
   output reg [7:0] data_out);
 
-reg [7:0] mem [0:1919];
+reg [7:0] mem [0:2399];
 
 always @(posedge clk) begin
-  if (we && w_addr < 1920) mem[w_addr] <= data_in;
+  if (we && w_addr < 2400) mem[w_addr] <= data_in;
   data_out <= mem[r_addr];
 end
 
@@ -139,11 +145,12 @@ module font8x16 (
 		 output pixel
 		 );
 
-reg [127:0] fnt [0:255];
+reg [0:127] fnt [0:255];
 initial $readmemh("../../mod_vga_text/font.hex", fnt);
 
-wire [7:0] r = row;
+wire [6:0] r = row;
+wire [6:0] c = col;
 
-assign pixel = fnt[ascii_code][(r << 3) + ~col];
+assign pixel = (row != 0) & fnt[ascii_code][(r << 3) - c] ;
 	 
 endmodule
